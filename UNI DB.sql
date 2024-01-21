816a38d355d3c3cc8d03407e6718a312d36c0fbf
Heshan Chandeepa <118461284+HChandeepa@users.noreply.github.com>
2024-01-21 10:39:30 +0530

    student table created

--- UNI DB.sql
+++ UNI DB.sql
@@ -1,3 +1,13 @@
 create database Uni;
 use Uni;
 
+create table Student(
+StudentID int not null Identity(0,1),
+NIC varchar(13),
+Name varchar(200),
+Address varchar(200),
+DOB varchar(15),
+Email varchar(100),
+Password varchar(45),
+primary key(StudentID)
+);
